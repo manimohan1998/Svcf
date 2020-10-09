49fa667f93eb48440c15befb7e63e91d870f5db5
salini@1997 <salinimahendran@gmail.com>
2020-10-09 13:34:35 +0530

    login update

--- src/app/Login/Auth/auth.guard.ts
+++ src/app/Login/Auth/auth.guard.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(public router: Router){}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('token')!=undefined){
+        this.router.navigate(['/subscribe-list'])
+        return true;
+      }else{
+        this.router.navigate(['/login']);
+      }
     return true;
   }
   
--- src/app/Login/login/login.page.ts
+++ src/app/Login/login/login.page.ts
@@ -10,50 +10,46 @@ import {Dialogs} from '@ionic-native/dialogs/ngx'
 })
 export class LoginPage implements OnInit {
   loginForm: FormGroup;
- emp=false;
- cus=false;
-  user: string;
+  emp=false;
+  cus=false;
+  user: string
+  logindata:{};
   constructor(private fb:FormBuilder,private network:Network,private dialogs:Dialogs,private router:Router) { 
     this.loginForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      password: ['', [Validators.required, Validators.minLength(5)]],
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(4)]],
    })
   }
   ngOnInit() {
-    this.network.onDisconnect().subscribe(()=>{
+      this.network.onDisconnect().subscribe(()=>{
       this.dialogs.alert('Network was disconnected')
           })
-          this.network.onConnect().subscribe(()=>{
-            setTimeout(()=>{
+      this.network.onConnect().subscribe(()=>{
+       setTimeout(()=>{
       this.dialogs.alert('we got a '+this.network.type+'connection');
             },2000);
-          })
+      })
   }
   employee(){
-this.emp=true;
-this.cus=false;
-localStorage.setItem('user',"employee");
-this.loginForm.reset();
-}
-  customer(){
-this.cus=true;
-this.emp=false;
-localStorage.setItem('user',"customer");
-this.loginForm.reset();
+        this.emp=true;
+        this.cus=false;
+        localStorage.setItem('user',"employee");
+        this.loginForm.reset();
   }
+  customer(){
+        this.cus=true;
+        this.emp=false;
+        localStorage.setItem('user',"customer");
+        this.loginForm.reset();
+   }
 
-  submitForm(){
-    console.log(this.loginForm.value)
-    console.log(localStorage.getItem('user'))
-    this.user=localStorage.getItem('user')
-if(this.cus==true && this.emp==false && this.user=="customer"){
-  this.router.navigate(['/subscribe-list'])
-}
- if(this.cus==false && this.emp==true && this.user=="employee"){
-  this.router.navigate(['/customer-list'])
-}
-}
+  submitForm(val){
+        this.logindata={username:val.name,password:val.password}
+        console.log(this.logindata)
+        this.router.navigate(["/subscribe-list"])
+ }
+
   Forgot(){
-    this.router.navigate(['/forgot-password'])
+      this.router.navigate(['/forgot-password'])
   }
 }
