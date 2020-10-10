6e6f794cc44c2a54ce8729bd9121b49bba30d268
salini@1997 <salinimahendran@gmail.com>
2020-10-10 12:35:38 +0530

    update

--- src/app/subscribers/subscriber-api.service.ts
+++ src/app/subscribers/subscriber-api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriberApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  subscriberList(sub_id){
+    return this.http.get(environment.url+'set backend endpoint?optvalue='+sub_id)
+  }
+
+  makepayment(payment_detail){
+    return this.http.post(environment.url+'set backend endpoint',payment_detail)
+  }
+
+  ReceiveRecipt(sub_id){
+    return this.http.get(environment.url+'set backend endpoint?optvalue='+sub_id)
+  }
+  
 }
